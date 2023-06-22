0331df15242dbbbb307002d0110767c8e07ff457
PietroMarilei~ <pietromrl0@gmail.com>
2023-06-22 12:10:13 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+
+const buttonElement = document.getElementById("play_button");
+const selectElement = document.getElementById("difficulty");
+
+const containerElement = document.getElementById('cell_container')
+
+let numbOfCells
+const bombsArray = []
+//le bombe sono 16 di default
+
+bombGenerator()
+
+buttonElement.addEventListener('click', function () {
+    containerElement.innerHTML = ''
+    // questo affare svuota la tabella prima che succeda altro
+
+    cellCreator(numbOfCells);
+
+})
+
+/*
+FUNCTIONS-------------------------------
+*/
+
+function cellCreator(numbOfCells) {
+    numbOfCells = selectElement.value
+    //prendo il numero delle celle selezionate
+    let gridLength = Math.sqrt(numbOfCells);
+
+    for (let i = 1; i <= numbOfCells; i++) {
+        console.log('creato cella');
+
+        const singleCellEl = document.createElement('div');
+        singleCellEl.innerHTML = i;
+        singleCellEl.classList.add('cell');
+        singleCellEl.style.width = `calc(100% / ${gridLength})`;
+
+        // add clickable
+
+        singleCellEl.addEventListener('click', function (e) {
+            this.classList.toggle('active');
+            console.log('cliccato cella num ', this.innerText);
+        })
+
+        containerElement.append(singleCellEl);
+    }
+
+}
+
+// function bombGenerator() {
+//     let y = 0
+//     while (y < 16) {
+//         console.log('bomba', y);
+
+//         y++
+//     }
+// }
